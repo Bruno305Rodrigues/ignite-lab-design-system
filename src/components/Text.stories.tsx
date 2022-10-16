a47395ef942fd1d084feb8a774baeabf05538ea6
Bruno305Rodrigues <bruno_lmr_op@hotmail.com>
2022-10-16 15:45:25 -0300

    initial commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Laurem'
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    },
+
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Testando</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true
+            }
+        },
+        asChild: {
+            table: {
+                disable: true,
+            }
+        }
+    }
+}
